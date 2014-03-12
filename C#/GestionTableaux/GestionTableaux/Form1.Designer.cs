acb333dd3f2d49d5e97fa26c9d611e4caf543be1
Issame <issame.kastite@gmail.com>
2014-03-12 23:40:49 +0000

    - tri et tableaux en C#

---
+++ C#/GestionTableaux/GestionTableaux/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace GestionTableaux
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T0 = new System.Windows.Forms.TextBox();
+            this.T1 = new System.Windows.Forms.TextBox();
+            this.T2 = new System.Windows.Forms.TextBox();
+            this.T3 = new System.Windows.Forms.TextBox();
+            this.T4 = new System.Windows.Forms.TextBox();
+            this.T5 = new System.Windows.Forms.TextBox();
+            this.T6 = new System.Windows.Forms.TextBox();
+            this.T7 = new System.Windows.Forms.TextBox();
+            this.T8 = new System.Windows.Forms.TextBox();
+            this.T9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // T0
+            // 
+            this.T0.Location = new System.Drawing.Point(24, 25);
+            this.T0.Name = "T0";
+            this.T0.Size = new System.Drawing.Size(34, 20);
+            this.T0.TabIndex = 1;
+            this.T0.Text = "4";
+            // 
+            // T1
+            // 
+            this.T1.Location = new System.Drawing.Point(66, 25);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(34, 20);
+            this.T1.TabIndex = 2;
+            this.T1.Text = "8";
+            // 
+            // T2
+            // 
+            this.T2.Location = new System.Drawing.Point(108, 25);
+            this.T2.Name = "T2";
+            this.T2.Size = new System.Drawing.Size(34, 20);
+            this.T2.TabIndex = 3;
+            this.T2.Text = "9";
+            // 
+            // T3
+            // 
+            this.T3.Location = new System.Drawing.Point(150, 25);
+            this.T3.Name = "T3";
+            this.T3.Size = new System.Drawing.Size(34, 20);
+            this.T3.TabIndex = 4;
+            this.T3.Text = "2";
+            // 
+            // T4
+            // 
+            this.T4.Location = new System.Drawing.Point(192, 25);
+            this.T4.Name = "T4";
+            this.T4.Size = new System.Drawing.Size(34, 20);
+            this.T4.TabIndex = 5;
+            this.T4.Text = "1";
+            // 
+            // T5
+            // 
+            this.T5.Location = new System.Drawing.Point(234, 25);
+            this.T5.Name = "T5";
+            this.T5.Size = new System.Drawing.Size(34, 20);
+            this.T5.TabIndex = 6;
+            this.T5.Text = "10";
+            // 
+            // T6
+            // 
+            this.T6.Location = new System.Drawing.Point(276, 25);
+            this.T6.Name = "T6";
+            this.T6.Size = new System.Drawing.Size(34, 20);
+            this.T6.TabIndex = 7;
+            this.T6.Text = "115";
+            // 
+            // T7
+            // 
+            this.T7.Location = new System.Drawing.Point(318, 25);
+            this.T7.Name = "T7";
+            this.T7.Size = new System.Drawing.Size(34, 20);
+            this.T7.TabIndex = 8;
+            this.T7.Text = "0";
+            // 
+            // T8
+            // 
+            this.T8.Location = new System.Drawing.Point(360, 25);
+            this.T8.Name = "T8";
+            this.T8.Size = new System.Drawing.Size(34, 20);
+            this.T8.TabIndex = 9;
+            this.T8.Text = "92";
+            // 
+            // T9
+            // 
+            this.T9.Location = new System.Drawing.Point(402, 25);
+            this.T9.Name = "T9";
+            this.T9.Size = new System.Drawing.Size(34, 20);
+            this.T9.TabIndex = 10;
+            this.T9.Text = "99";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Trier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 122);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.T9);
+            this.Controls.Add(this.T8);
+            this.Controls.Add(this.T7);
+            this.Controls.Add(this.T6);
+            this.Controls.Add(this.T5);
+            this.Controls.Add(this.T4);
+            this.Controls.Add(this.T3);
+            this.Controls.Add(this.T2);
+            this.Controls.Add(this.T1);
+            this.Controls.Add(this.T0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox T0;
+        private System.Windows.Forms.TextBox T1;
+        private System.Windows.Forms.TextBox T2;
+        private System.Windows.Forms.TextBox T3;
+        private System.Windows.Forms.TextBox T4;
+        private System.Windows.Forms.TextBox T5;
+        private System.Windows.Forms.TextBox T6;
+        private System.Windows.Forms.TextBox T7;
+        private System.Windows.Forms.TextBox T8;
+        private System.Windows.Forms.TextBox T9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C#/GestionTableaux/GestionTableaux/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GestionTableaux
+{
+    public partial class Form1 : Form
+    {
+        int[] t = new int[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void lire()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                t[i] = Convert.ToInt32(this.Controls["t" + i].Text);
+            }
+        }
+
+        public void ecrire()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this.Controls["t" + i].Text = t[i].ToString();
+            }
+        }
+
+        public void trisel()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (t[i] > t[j])
+                    {
+                        int tmp = t[i];
+                        t[i] = t[j];
+                        t[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lire();
+
+            trisel();
+
+            ecrire();
+
+        }
+    }
+}
